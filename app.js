a56da48507575f519ca8e444ae261b4f199e7e01
AneeshPal <2k23.cs2313398@gmail.com>
2026-01-19 13:27:22 +0530

    added signup and login functionality

--- app.js
+++ app.js
@@ -7,10 +7,18 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync");
 const ExpressError = require("./utils/ExpressError.js");
-const listings=require("./routes/listing.js");
-const reviews=require("./routes/review.js");
-const session=require("express-session");
 
+
+const listingRouter=require("./routes/listing.js");
+const reviewRouter=require("./routes/review.js");
+const userRouter=require("./routes/user.js");
+
+
+const session=require("express-session");
+const flash=require("connect-flash");
+const passport=require("passport");
+const LocalStrategy=require("passport-local");
+const User=require("./models/user.js")
 
 
 //Database connection
@@ -48,9 +56,28 @@ const sessionOptions={
   }
 }
 app.use(session(sessionOptions));
+app.use(flash());
 
-app.use("/listings",listings);
-app.use("/listings/:id/reviews",reviews);
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.use((req,res,next)=>{
+  res.locals.success=req.flash("success");
+  res.locals.error=req.flash("error");
+  res.locals.currUser=req.user;
+  next();
+});
+
+
+
+app.use("/listings",listingRouter);
+app.use("/listings/:id/reviews",reviewRouter);
+app.use("/",userRouter);
 
 
 
--- routes/listing.js
+++ routes/listing.js
@@ -4,6 +4,8 @@ const wrapAsync = require("../utils/wrapAsync");
 const ExpressError = require("../utils/ExpressError.js");
 const { listingSchema, reviewSchema } = require("../schema.js");
 const Listing = require("../models/listing.js");
+const {isLoggedIn} = require("../middleware.js");
+
 
 
 //SchemaValidation Middleware
@@ -27,7 +29,7 @@ router.get("/", wrapAsync(async (req, res) => {
 
 
 //New Route
-router.get("/new", (req, res) => {
+router.get("/new",isLoggedIn, (req, res) => {
   res.render("listings/new.ejs");
 });
 
@@ -35,6 +37,10 @@ router.get("/new", (req, res) => {
 router.get("/:id", wrapAsync(async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id).populate("reviews");
+  if(!listing){
+    req.flash("error","Listing you requested does not exists");
+    res.redirect("/listings");
+  }
   res.render("listings/show.ejs", { listing });
 }));
 
@@ -44,29 +50,32 @@ router.post("/", validateListing, wrapAsync(async (req, res, next) => {
 
   const newListing = new Listing(req.body.listing);
   await newListing.save();
+  req.flash("success","New Listing Created!");
   res.redirect("/listings");
 })
 );
 
 //Edit Route
-router.get("/:id/edit", wrapAsync(async (req, res) => {
+router.get("/:id/edit", isLoggedIn,wrapAsync(async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("listings/edit.ejs", { listing });
 }));
 
 //Update Route
-router.put("/:id", validateListing, wrapAsync(async (req, res) => {
+router.put("/:id", isLoggedIn,validateListing, wrapAsync(async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  req.flash("success","Updated Listing");
   res.redirect("/listings");
 }));
 
 //Delete Route
-router.delete("/:id", wrapAsync(async (req, res) => {
+router.delete("/:id",isLoggedIn, wrapAsync(async (req, res) => {
   let { id } = req.params;
   let deletedListing = await Listing.findByIdAndDelete(id);
   console.log(deletedListing);
+  req.flash("success","Listing Deleted");
   res.redirect("/listings");
 }));
 
